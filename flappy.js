3a6e69f91f2e741cd9e5169b0889b98ef7c88554
Rafael Machado <114411912+rafaelmachado18@users.noreply.github.com>
2023-10-06 08:34:34 -0300

    Add files via upload
    Agora os canos e suas movimentações foram adicionados, como também a tecla "ALT" não tira mais o foco do jogo.

--- flappy.js
+++ flappy.js
@@ -27,10 +27,37 @@ const cap = (up_cap, down_cap) => (pos, height) => {
     return pos
 }
 
+//mapeia um valor de zero a um para um valor dentro do intervalo especificado(utilizado para trabalhar com numeros aleatorios)
+const map = (up, down) => (value) => value*(up - down) + down
+
+//cria um cano
+const pipe = (x, y, inverted) => {
+    return {
+        spriteX: inverted ? 52 : 0,
+        spriteY: 169,
+        swidth: 52,
+        sheight: 400,
+        width: 52,
+        height: 400,
+        x: x,
+        y: y
+    }
+}
+
+//cria um par de canos(um normal e um de cabeça para baixo)
+const pipe_pair = (x, y, gap) => {
+    return {
+        floor_pipe: pipe(x, y, false),
+        sky_pipe: pipe(x, y - gap - 400, true)
+    }
+}
+
 // gera um novo objeto de jogador(player) atualizando sua posição com base na cena do jogo, no tempo passado, e na tecla pressionada
 const next_player = (execution) => (state) => {
     if(state.scene === "play") {
-        return merge(state.player)({y: cap(state.canvas.height - state.floor.height, 0)(state.player.y + 0.25*execution.dt - 20*(execution.keyboard.key === "w"), 24)})
+      const new_y =   cap(state.canvas.height - state.floor.height, 0)(state.player.y +state.player.v*execution.dt, 24)
+      const new_v = (execution.keyboard.key === "w") ? -0.5 : state.player.v + execution.dt*0.002
+      return merge(state.player)({y: new_y, v: new_v})
     }
     return {...state.player}
 }
@@ -46,14 +73,44 @@ const next_scene = (execution) => (state) => {
     return "play"
 }
 
+//Move um objeto do cenário do jogo(utilizado para mover o plano de fundo e o chão)
+const move_scenario_object = (name) => (execution) => (state) => {
+    if(state[name].x <= -state[name].width) {
+        return merge(state[name])({x: 0})
+    }
+    return merge(state[name])({x: state[name].x - state.speed*execution.dt})
+}
+
+//Move um cano
+const move_pipe = (speed) => (dt) => (selected_pipe) => {
+    return merge(selected_pipe)({x: selected_pipe.x - speed*dt})
+}
+
+//atualiza todos os canos da tela(mudando suas posições)
+const update_pipes = (execution) => (state) => {
+    if(state.scene === "play")
+    {
+        const new_gap = map(80, 160)(execution.seed)
+        const new_pos = map(canvas.height/5, 4*canvas.height/5)(execution.seed)
+        const updated_clock = state.pipes.clock + execution.dt
+        const added_pairs = updated_clock > 1500 ? [...state.pipes.pairs, pipe_pair(canvas.width, new_pos, new_gap)] : [...state.pipes.pairs]
+        const moved_pairs = added_pairs.map((x) => merge(x)({floor_pipe: move_pipe(state.speed)(execution.dt)(x.floor_pipe), sky_pipe: move_pipe(state.speed)(execution.dt)(x.sky_pipe)}))
+        //const on_screen_pairs = moved_pairs.filter((x) => x.floor_pipe.x + 52 < 0)
+        const new_clock = updated_clock > 1500 ? 0 : updated_clock
+        return {pairs: [...moved_pairs], clock: new_clock}
+    }
+    return {...state.pipes}
+}
 //gera um novo estado do jogo
 const next_state = specEvent({
     canvas: (execution) => (state) => state.canvas,
     context: (execution) => (state) => state.context,
     spritesheet: (execution) => (state) => state.spritesheet,
-    floor: (execution) => (state) => state.floor,
-    background: (execution) => (state) => state.background,
     initial_screen: (execution) => (state) => state.initial_screen,
+    speed: (execution) => (state) => state.speed,
+    pipes: update_pipes,
+    floor: move_scenario_object('floor'),
+    background: move_scenario_object('background'),
     player: next_player,
     scene: next_scene
 })
@@ -62,6 +119,12 @@ const next_state = specEvent({
 const draw_game = (state) => {
     draw_game_object(state.context)(state.spritesheet)(state.background)
     draw_game_object(state.context)(state.spritesheet)(state.floor)
+    draw_game_object(state.context)(state.spritesheet)(merge(state.background)({x: state.background.x + state.background.width}))
+    draw_game_object(state.context)(state.spritesheet)(merge(state.floor)({x: state.floor.x + state.floor.width}))
+    state.pipes.pairs.map((x) => {
+        draw_game_object(state.context)(state.spritesheet)(x.floor_pipe)
+        draw_game_object(state.context)(state.spritesheet)(x.sky_pipe)
+    })
     if(state.scene === "play"){
         draw_game_object(state.context)(state.spritesheet)(state.player)
     }
@@ -71,10 +134,11 @@ const draw_game = (state) => {
 }
 
 //estado inicial do jogo
-const game = Object.freeze({
+let game = {
     canvas : Object.freeze(document.getElementById('canvas')),
     context : Object.freeze(canvas.getContext('2d')),
     spritesheet: Object.freeze(sprite),
+    speed: 0.25,
     scene: "start",
     floor: {
         spriteX: 0,
@@ -94,7 +158,8 @@ const game = Object.freeze({
         width: 33,
         height: 24,
         x: canvas.width/2 - 16.5,
-        y: 50
+        y: 50,
+        v: 0
     },
     background : {
         spriteX: 390,
@@ -115,19 +180,21 @@ const game = Object.freeze({
         height: 152,
         x: (canvas.width / 2) - 174 / 2,
         y: 50
-    }
-})
+    },
+    pipes: {pairs: [pipe_pair(canvas.width, canvas.height/2, 80)], clock: 0}
+}
 
 // trecho não funcional
 //variável que armazena quando a tecla "w" é apertada e a variação de tempo entre um frame e outro
-let global_event = {dt: 0, keyboard: {key: ""}}
+let global_event = {dt: 0, keyboard: {key: ""}, seed: Math.random()}
 
 //atualiza os frames e o estado do jogo
-const loop = (state) => (t1) => (t2) => {
+const loop = (t1) => (t2) => {
     global_event.dt = t2 - t1
-    const new_state = next_state(global_event)(state)
-    draw_game(state)
-    window.requestAnimationFrame(loop(new_state)(t2))
+    global_event.seed = Math.random()
+    game = next_state(global_event)(game)
+    draw_game(game)
+    window.requestAnimationFrame(loop(t2))
 }
 
 //atualiza a variável global quando a tecla "w" é apertada e solta
@@ -141,6 +208,12 @@ window.addEventListener("keyup", (e) => {
         global_event.keyboard = {key: ""}
     }
 })
+//impede a tecla alt de tirar o foco do jogo
+window.addEventListener('keydown', (e) => {
+    if (e.altKey) {
+      e.preventDefault();
+    }
+  });
 
 //roda o jogo
-loop(game)(0)(0)
+loop(0)(0)
